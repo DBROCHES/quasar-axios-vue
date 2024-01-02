385a7b616657787154227eac8b674683be582550
Franco <francoseeker@gmail.com>
2024-01-02 01:45:02 -0500

    cambios pantalla nueva

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -157,14 +157,6 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-
-    <q-footer bordered class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
   </q-layout>
 </template>
 
